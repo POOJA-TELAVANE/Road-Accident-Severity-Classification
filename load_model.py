76e6c12859c77ab311345163184329e9772a042e
Pooja Telavane <74729744+POOJA-TELAVANE@users.noreply.github.com>
2022-03-22 23:00:34 +0400

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -1,19 +1,20 @@
 import joblib
 import requests
-
+import os
 
 def get_model(model_path):
+    
     try:
         with open(model_path, "rb") as mh:
             rf = joblib.load(mh)
     except:
         print("Cannot fetch model from local downloading from drive")
-
-        # example url: "https://drive.google.com/u/1/uc?id=18IxYOI-whucBTZmt5qTvvYgjlxleaSqO&export=download"
-        url = "https://drive.google.com/u/0/uc?id=1eZQFdqwAMHLsjwD2C-PyvcZlVAwmAudD&export=download"
-        r = requests.get(url, allow_redirects=True)
-        open(r"RTA_severity model.pkl", 'wb').write(r.content)
-        del r
+        if not 'RTA_severity model.pkl' in os.listdir('.'):
+            # example url: "https://drive.google.com/u/1/uc?id=18IxYOI-whucBTZmt5qTvvYgjlxleaSqO&export=download&confirm=t"
+            url = "https://drive.google.com/u/0/uc?id=1eZQFdqwAMHLsjwD2C-PyvcZlVAwmAudD&export=download&confirm=t"
+            r = requests.get(url, allow_redirects=True)
+            open(r"RTA_severity model.pkl", 'wb').write(r.content)
+            del r
         with open(r"RTA_severity model.pkl", "rb") as m:
             rf = joblib.load(m)
     return rf
