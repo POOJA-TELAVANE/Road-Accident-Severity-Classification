108fb122ffc5ca9c5fcd5536d98ef0c6301bc571
Pooja Telavane <74729744+POOJA-TELAVANE@users.noreply.github.com>
2022-03-22 19:37:19 +0400

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -12,7 +12,7 @@ def get_model(model_path):
         # example url: "https://drive.google.com/u/1/uc?id=18IxYOI-whucBTZmt5qTvvYgjlxleaSqO&export=download"
         url = "https://drive.google.com/u/0/uc?id=1eZQFdqwAMHLsjwD2C-PyvcZlVAwmAudD&export=download"
         r = requests.get(url, allow_redirects=True)
-        open(r"model.pkl", 'wb').write(r.content)
+        open(r"RTA_severity model.pkl", 'wb').write(r.content)
         del r
         with open(r"RTA_severity model.pkl", "rb") as m:
             rf = joblib.load(m)
